1c9b661c59d93a7320de4780f5b8b013acdb9a83
steppi <albert.steppi@gmail.com>
2024-08-16 07:25:14 -0400

    Add documentation for branch creation script

--- scripts/create_branch_for_language.sh
+++ scripts/create_branch_for_language.sh
@@ -1,5 +1,25 @@
 #usr/bin/env bash
 
+# Given a GitHub repository which has been synced to Crowdin, this script
+# is used to create a new branch including all and only commits from Crowdin's
+# branch for a particular language of interest. It is useful because
+
+# 1. Crowdin's branch contains commits for all languages under translation,
+#    but we prefer to be able to create, review, and merge pull requests for
+#    one language at a time.
+#
+# 2. Crowdin's branch can only be edited through the Crowdin UI (commits
+#    pushed to it outside of Crowdin will be lost when the branch is synced
+#    to Crowdin. There are times when translations must be edited outside of
+#    Crowdin due to incorrect segmentation of the content into strings for
+#    translation (e.g. html embedded within markdown documents getting
+#    garbled).
+
+# This script must be run inside of the repository of interest. At the
+# end, the newly created branch will be checked out. As a side effect,
+# the local source branch and the corresponding local Crowdin branch
+# (e.g. main and  l10n_main respectively) will become up to date with the
+# upstream remote (e.g. numpy/main and numpy/l10n_main for numpy.org).
 
 
 # Clean up by aborting the rebase and deleting the new branch if there
@@ -10,7 +30,7 @@ cleanup() {
     echo "Performing cleanup..."
     if git branch --list | grep -q "$new_branch"; then
         git rebase --abort || true
-        git checkout "$target_branch"
+        git checkout "$source_branch"
         git branch -D "$new_branch"
     fi
 }
@@ -20,23 +40,23 @@ trap cleanup EXIT
 
 # Check arguments
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
-  echo "Usage: $0 <upstream_remote> <target_branch> <crowdin_branch> <language_code>"
+  echo "Usage: $0 <upstream_remote> <source_branch> <crowdin_branch> <language_code>"
   exit 1
 fi
 
 upstream_remote=$1
-target_branch=$2
+source_branch=$2
 crowdin_branch=$3
 language_code=$4
 
 script_location=$(dirname "$0")
 
-# Make sure target branch is up to date with upstream
-git checkout $target_branch
+# Make sure source branch is up to date with upstream
+git checkout $source_branch
 git fetch $upstream_remote
-git merge --ff-only "${upstream_remote}/${target_branch}"
+git merge --ff-only "${upstream_remote}/${source_branch}"
 
-# Make sure l10n_main is up to date
+# Make sure the corresponding Crowdin branch is up to date
 git checkout $crowdin_branch
 git merge --ff-only "${upstream_remote}/${crowdin_branch}"
 
@@ -58,7 +78,7 @@ from git_tools import filter_commits
 
 filter_commits('\$filename', '$language_code')
 \"
-}; f" git rebase -i "$target_branch"
+}; f" git rebase -i "$source_branch"
 
 # Remove the trap if the rebase succeeds. No clean up is necessary.
 trap - EXIT
